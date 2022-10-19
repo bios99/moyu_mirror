ba5ad63dfd0200fda27613d10546cdf1629c0ba8
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-10-19 18:15:21 +0800

    Update baidulibrary.js

--- Crack/baidulibrary.js
+++ Crack/baidulibrary.js
@@ -1,12 +1,12 @@
 /***********************************
 
-> 应用名称：百度文库☆解锁VIP文档阅读权限
+> 应用名称：百度文库
+> 脚本功能：解锁VIP文档阅读权限
 > 脚本作者：Cuttlefish
 > 微信账号：墨鱼手记
 > 更新时间：2022-10-19
 > 通知频道：https://t.me/ddgksf2021
 > 投稿助手：https://t.me/ddgksf2013_bot
-> 脚本功能：解锁VIP文档阅读权限
 > 问题反馈：📮 ddgksf2013@163.com 📮
 > 特别说明：⛔⛔⛔
             本脚本仅供学习交流使用，禁止转载、售卖
@@ -24,5 +24,5 @@ hostname=appwk.baidu.com
 ***********************************/
 
 let ddgksf2013 = JSON.parse($response.body);
-ddgksf2013.data.vip.edu_vip_info={"uid":12345678,"type":2,"start_time":1622222200,"end_time":4622222200,"is_vip":1,"remain_day":99,"pro_total":8,"normal_total":8};
+ddgksf2013.data.vip.base_vip_info={"uid":12345678,"type":2,"start_time":1622222200,"end_time":4622222200,"is_vip":1,"remain_day":999,"pro_total":5,"normal_total":5};
 $done({ body: JSON.stringify(ddgksf2013) });
